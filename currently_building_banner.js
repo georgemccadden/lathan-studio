d02ff0c4cc3eb9d85ccce50edaa5f10821fc999d
George McCadden III <thethirdone.g@gmail.com>
2021-08-09 16:30:40 -0700

    cbuilding a like button
    this will be used as a way to see if viewers are interested in what is being built

---
+++ currently_building_banner.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const e = React.createElement;
+
+class currentlyBuilding extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { liked: false };
+	}
+
+	render() {
+		if (this.state.liked) {
+			return 'You liked this.';
+		}
+
+		return e('button', { onClick: () => this.setState({ liked: true }) }, 'Like');
+	}
+}
+
+const domContainer = document.querySelector('#currently_building_banner');
+ReactDOM.render(e(currentlyBuilding), domContainer);
